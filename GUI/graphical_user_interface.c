e815b784fd82bbd16262f827e8e08b4e59259a49
Kamilq99 <thealti222@gmail.com>
2024-11-12 20:03:42 +0100

    Combining window application with license key verification.

--- GUI/graphical_user_interface.c
+++ GUI/graphical_user_interface.c
@@ -1,10 +1,15 @@
 #include <windows.h>
+#include <stdio.h>
 
-// Deklaracja funkcji
+// Function prototype for the license verification function in VerifyKey.dll
+typedef bool (*VerifyKeyFunc)(const char* key);
+
+// Window procedure declaration
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
+// Entry point for the Windows application
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    // Zarejestrowanie klasy okna
+    // Register the window class
     const char CLASS_NAME[] = "LicenseKeyWindow";
 
     WNDCLASS wc = { };
@@ -14,7 +19,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
     RegisterClass(&wc);
 
-    // Stworzenie okna
+    // Create the window
     HWND hwnd = CreateWindowEx(
         0,
         CLASS_NAME,
@@ -33,7 +38,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
     ShowWindow(hwnd, nCmdShow);
 
-    // Pętla wiadomości
+    // Message loop
     MSG msg = { };
     while (GetMessage(&msg, NULL, 0, 0)) {
         TranslateMessage(&msg);
@@ -43,13 +48,15 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     return 0;
 }
 
-// Funkcja okna (window procedure) obsługująca wiadomości
+// Window procedure to handle messages
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     static HWND hLabel, hEdit, hButton;
+    static HINSTANCE hLib = NULL;
+    static VerifyKeyFunc verify_key = NULL;
 
     switch (uMsg) {
         case WM_CREATE:
-            // Etykieta z opisem
+            // Create the label
             hLabel = CreateWindow(
                 "STATIC", 
                 "Enter your license key below", 
@@ -61,7 +68,7 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
                 NULL
             );
 
-            // Pole tekstowe do wpisania klucza
+            // Create the text box for entering the key
             hEdit = CreateWindow(
                 "EDIT", 
                 "", 
@@ -73,7 +80,7 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
                 NULL
             );
 
-            // Przycisk "Verify"
+            // Create the "Verify" button
             hButton = CreateWindow(
                 "BUTTON", 
                 "Verify", 
@@ -84,17 +91,41 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
                 NULL, 
                 NULL
             );
+
+            // Load the VerifyKey.dll dynamically
+            hLib = LoadLibrary("VerifyKey/VerifyKey.dll");
+            if (hLib != NULL) {
+                // Get the address of the verify_license_key function
+                verify_key = (VerifyKeyFunc)GetProcAddress(hLib, "verify_license_key");
+                if (verify_key == NULL) {
+                    MessageBox(hwnd, "Could not find the function in the DLL", "Error", MB_OK);
+                }
+            } else {
+                MessageBox(hwnd, "Could not load VerifyKey.dll", "Error", MB_OK);
+            }
             break;
 
         case WM_COMMAND:
-            if (LOWORD(wParam) == 1) {  // Obsługa przycisku "Verify"
+            if (LOWORD(wParam) == 1) {  // Handle the "Verify" button click
                 char licenseKey[256];
-                GetWindowText(hEdit, licenseKey, 256);
-                MessageBox(hwnd, licenseKey, "Entered License Key", MB_OK);
+                GetWindowText(hEdit, licenseKey, 256);  // Get the entered license key
+
+                if (verify_key != NULL) {
+                    // Call the verify_license_key function from the DLL
+                    if (verify_key(licenseKey)) {
+                        MessageBox(hwnd, "License key is valid!", "Success", MB_OK);
+                    } else {
+                        MessageBox(hwnd, "Invalid license key!", "Error", MB_OK);
+                    }
+                }
             }
             break;
 
         case WM_DESTROY:
+            // Free the loaded DLL before exiting
+            if (hLib != NULL) {
+                FreeLibrary(hLib);
+            }
             PostQuitMessage(0);
             return 0;
     }
